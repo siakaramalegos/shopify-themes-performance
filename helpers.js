9074a1ec945c998554dfc0f287fa460d765c7e1d
Sia Karamalegos <siakaramalegos@gmail.com>
2024-09-11 11:14:05 -0400

    Move heavy lift to node script, generate rest of CWV charts

--- helpers.js
+++ helpers.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const echarts = require('echarts');
 
 // get data from json file
 function readDataFile(readFilePath) {
@@ -21,6 +22,7 @@ function getDateFileString(date) {
 
 function mungeData(origins, rawPercents, dateString, client) {
   const convertedPercents = {}
+
   Object.keys(rawPercents).forEach(key => {
     convertedPercents[key] = Math.round(parseFloat(rawPercents[key]) * 1000) / 10
   })
@@ -28,7 +30,7 @@ function mungeData(origins, rawPercents, dateString, client) {
   return {
     date: dateString,
     client,
-    origins,
+    origins: parseInt(origins, 10),
     ...convertedPercents,
   }
 }
@@ -47,9 +49,155 @@ function writeDataFile(data, cacheDir, writeFilePath, descriptor) {
   })
 }
 
+// Green-red stacked bar chart
+function getPassingCwvSvg(monthlyData, currentMonthsReadable) {
+  // In SSR mode the first container parameter is not required
+  let chart = echarts.init(null, null, {
+    renderer: 'svg', // must use SVG rendering mode
+    ssr: true, // enable SSR
+    width: 300, // need to specify height and width
+    height: 300
+  });
+
+  chart.setOption({
+    xAxis: {
+      data: currentMonthsReadable
+    },
+    yAxis: {},
+    series: [
+      {
+        data: monthlyData,
+        type: 'bar',
+        stack: 'x',
+        label: {
+          show: true,
+          position: 'insideTop'
+        },
+        itemStyle: {
+          color: '#00916eff',
+          borderColor: '#fff',
+        }
+      },
+      {
+        data: monthlyData.map(x => 100 - x),
+        type: 'bar',
+        stack: 'x',
+        itemStyle: {
+          color: '#e01a4c',
+          borderColor: '#fff',
+        }
+      }
+    ]
+  });
+
+  const svgStr = chart.renderToSVGString();
+
+  // Disposing chart to release memory.
+  chart.dispose();
+  chart = null;
+
+  return svgStr
+}
+
+// Line chart (for origins by month)
+function getLineSvg(monthlyData, currentMonthsReadable) {
+  // In SSR mode the first container parameter is not required
+  let chart = echarts.init(null, null, {
+    renderer: 'svg', // must use SVG rendering mode
+    ssr: true, // enable SSR
+    width: 460, // need to specify height and width
+    height: 300
+  });
+
+  chart.setOption({
+    xAxis: {
+      type: 'category',
+      data: currentMonthsReadable
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        data: monthlyData,
+        type: 'line',
+      }
+    ]
+  });
+
+  const svgStr = chart.renderToSVGString();
+
+  // Disposing chart to release memory.
+  chart.dispose();
+  chart = null;
+
+  return svgStr
+}
+
+// Red-yellow-green stacked bar chart
+function getStackedBarSvg(passingMonthly, needsImproveMonthly, poorMonthly, currentMonthsReadable) {
+  let chart = echarts.init(null, null, {
+    renderer: 'svg', // must use SVG rendering mode
+    ssr: true, // enable SSR
+    width: 300, // need to specify height and width
+    height: 300
+  });
+
+  chart.setOption({
+    xAxis: {
+      data: currentMonthsReadable
+    },
+    yAxis: { max: 100 },
+    series: [
+      {
+        data: passingMonthly,
+        type: 'bar',
+        stack: 'x',
+        label: {
+          show: true,
+          position: 'insideTop'
+        },
+        itemStyle: {
+          color: '#00916eff',
+          borderColor: '#fff',
+        }
+      },
+      {
+        data: needsImproveMonthly,
+        type: 'bar',
+        stack: 'x',
+        itemStyle: {
+          color: '#ffcf00ff',
+          borderColor: '#fff',
+        }
+      },
+      {
+        data: poorMonthly,
+        type: 'bar',
+        stack: 'x',
+        itemStyle: {
+          color: '#e01a4c',
+          borderColor: '#fff',
+        }
+      }
+    ]
+  });
+
+  const svgStr = chart.renderToSVGString();
+
+  // Disposing chart to release memory.
+  chart.dispose();
+  chart = null;
+
+  return svgStr
+}
+
 module.exports = {
   readDataFile,
   getDateFileString,
   mungeData,
   writeDataFile,
+  getLineSvg,
+  getPassingCwvSvg,
+  getStackedBarSvg,
 }
--- processData.js
+++ processData.js
@@ -1,23 +1,39 @@
-const { readDataFile, getDateFileString, mungeData, writeDataFile } = require('./helpers');
+const { readDataFile, getDateFileString, mungeData, writeDataFile, getLineSvg, getPassingCwvSvg, getStackedBarSvg } = require('./helpers');
 const RAW_FOLDER = '_raw_data/';
 const CACHE_DIR = '_processed_data'
+const MIN_ORIGINS = 100
+const CLIENTS = ['mobile', 'desktop']
 
 const today = new Date()
-// TODO - change to -1
-const lastMonth = new Date(today.setMonth(today.getMonth() - 2))
+const lastMonth = new Date(today.setMonth(today.getMonth() - 1))
 
-const dateFileNames = Array(6).fill(0).map((_, i) => {
+// File names are YYYY_MM (e.g., 2024_07)
+const currentMonths = Array(6).fill(0).map((_, i) => {
   const newDate = lastMonth.setMonth(today.getMonth() - i)
   return getDateFileString(new Date(newDate))
-});
+}).sort();
+const currentMonthsReadable = currentMonths.map(dateStr => {
+  const parts = dateStr.split('_')
+  return `${parts[1]}/${parts[0].slice(2)}`
+})
 
 const confirmedThemes = readDataFile('currentThemes.json')
 
+// Initialize counters
 let themes = {}
 let ignoredEntries = 0
 let unconfirmedThemes = {}
+let allOtherOriginsCounts = { mobile: {}, desktop: {} }
+CLIENTS.forEach(client => {
+  currentMonths.forEach(monthStr => { allOtherOriginsCounts[client][monthStr] = 0 })
+})
+let totalOriginsCounts = { mobile: {}, desktop: {} }
+CLIENTS.forEach(client => {
+  currentMonths.forEach(monthStr => { totalOriginsCounts[client][monthStr] = 0 })
+})
 
-dateFileNames.forEach(date => {
+// For each monthly data file, dump each theme's monthly data into the theme object
+currentMonths.forEach(date => {
   const file = `${RAW_FOLDER}${date}.json`
   const fileData = readDataFile(file)
 
@@ -27,19 +43,21 @@ dateFileNames.forEach(date => {
   fileData.forEach(theme => {
     const {client, top_theme_name, origins, id, ...rawPercents} = theme
     const monthlyData = mungeData(origins, rawPercents, date, client)
+    totalOriginsCounts[client][monthlyData.date] = totalOriginsCounts[client][monthlyData.date] + monthlyData.origins
 
     // Set id to null string if null
     const theme_store_id = id || 'null'
-    // TODO: combine nulls and not counted into 1 object for total origins data
 
     if (!themes[theme_store_id]) {
       // early exit
-      if (monthlyData.origins < 30) {
+      if (monthlyData.origins < MIN_ORIGINS) {
         ignoredEntries++
+
+        allOtherOriginsCounts[client][monthlyData.date] = allOtherOriginsCounts[client][monthlyData.date] + monthlyData.origins
         return
       }
 
-      // check if confirmed theme
+      // check if confirmed theme, if so add it
       if (confirmedThemes[theme_store_id]) {
         const confirmedName = confirmedThemes[theme_store_id].name
         if (confirmedName !== top_theme_name){
@@ -52,30 +70,104 @@ dateFileNames.forEach(date => {
           slug: confirmedThemes[theme_store_id].slug,
           monthlyData: [ monthlyData ]
         }
-      } else if (!unconfirmedThemes[theme_store_id]) {
+      } else {
+        // If not confirmed, add to other origins count
+        allOtherOriginsCounts[client][monthlyData.date] = allOtherOriginsCounts[client][monthlyData.date] + monthlyData.origins
+
         // Add to unconfirmed themes list if not already there
-        unconfirmedThemes[theme_store_id] = top_theme_name
+        if (!unconfirmedThemes[theme_store_id]) {
+          unconfirmedThemes[theme_store_id] = top_theme_name
+        }
       }
     } else {
+      // Confirmed themes already in object, pass new month's data to it
       themes[id].monthlyData.push(monthlyData)
     }
   })
 
 })
 
-// Housekeeping
+// Housekeeping about bad theme names or theme_store_ids and general themes data
+console.log("**************************************************");
 if (Object.keys(unconfirmedThemes).length > 1) {
-  console.error("*****UNCONFIRMED THEMES*****");
-  console.log({unconfirmedThemes});
+  console.error("*** UNCONFIRMED THEMES:");
+  console.log("*** ", {unconfirmedThemes});
 }
 if (ignoredEntries > 0) {
-  console.log(`Ignored ${ignoredEntries} entries of unconfirmed themes with less than 5 origins`);
+  console.log(`*** Ignored ${ignoredEntries} entries of unconfirmed themes with less than ${MIN_ORIGINS} origins`);
 }
+console.log(`*** All other origins count is:`);
+console.log({allOtherOriginsCounts});
+console.log(`*** All origins count is:`);
+console.log({totalOriginsCounts});
 
+// Create the themes data object and SVG charts for use in page building
+console.log(`*** Creating charts for months: ${currentMonthsReadable.join(', ')}.`);
+
+const themesWithCharts = Object.keys(themes).map(themeId => {
+  const inputData = themes[themeId]
+  const {id, name, monthlyData, sunset, slug} = inputData
+  const metrics = ['origins', 'passingCWV', 'passingLCP', 'needsImproveLCP', 'poorLCP', 'passingCLS', 'needsImproveCLS', 'poorCLS', 'passingINP', 'needsImproveINP', 'poorINP']
+
+  // Initialize data object with null arrays. Array indexes are in date order.
+  const data = { mobile: {}, desktop: {} }
+  CLIENTS.forEach(client => {
+    metrics.forEach(metric => { data[client][metric] = Array(6).fill() })
+  })
+
+  monthlyData.forEach(dataset => {
+    const {client, date, origins, pct_good_cwv, pct_good_lcp, pct_ni_lcp, pct_poor_lcp, pct_good_cls, pct_ni_cls, pct_poor_cls, pct_good_inp, pct_ni_inp, pct_poor_inp} = dataset
+    const index = currentMonths.indexOf(date)
+    if (index) {
+      data[client].origins[index] = origins
+      if (origins > 30) {
+        data[client].passingCWV[index] = pct_good_cwv
+        data[client].passingLCP[index] = pct_good_lcp
+        data[client].needsImproveLCP[index] = pct_ni_lcp
+        data[client].poorLCP[index] = pct_poor_lcp
+        data[client].passingCLS[index] = pct_good_cls
+        data[client].needsImproveCLS[index] = pct_ni_cls
+        data[client].poorCLS[index] = pct_poor_cls
+        data[client].passingINP[index] = pct_good_inp
+        data[client].needsImproveINP[index] = pct_ni_inp
+        data[client].poorINP[index] = pct_poor_inp
+      }
+    }
+
+  })
+
+  const charts = {}
+
+  Object.keys(data).forEach(client => {
+    const {origins, passingCWV, passingLCP, needsImproveLCP, poorLCP, passingCLS, needsImproveCLS, poorCLS, passingINP, needsImproveINP, poorINP} = data[client]
+
+    charts[client] = {
+      originsSvg: getLineSvg(origins, currentMonthsReadable),
+      passingCwvSvg: getPassingCwvSvg(passingCWV, currentMonthsReadable),
+      passingCwvAria: `Origins Passing All Core Web Vitals bar chart. The data is: ${passingCWV.join(', ')}% passing for the months ${currentMonthsReadable.join(', ')}.`,
+      lcp: getStackedBarSvg(passingLCP, needsImproveLCP, poorLCP, currentMonthsReadable),
+      cls: getStackedBarSvg(passingCLS, needsImproveCLS, poorCLS, currentMonthsReadable),
+      inp: getStackedBarSvg(passingINP, needsImproveINP, poorINP, currentMonthsReadable),
+    }
+  })
+
+  return {
+    id,
+    name,
+    slug,
+    sunset,
+    data,
+    charts,
+  }
+})
+
+console.log(`*** ${themesWithCharts.length} themes generated.`);
+console.log("**************************************************");
 
 const output = {
-  themes,
-  dates: dateFileNames
+  themes: themesWithCharts,
+  dates: currentMonths,
+  readableDates: currentMonthsReadable,
 }
 
 const outputFileName = getDateFileString(new Date())
